8e3ac17cc8f2d942bbd193406a2d7aea7a595c4c
sachin-saara <shukla.s@saara.io>
2022-04-27 22:20:26 +0530

    finished the topic

--- constructor-function/base/index.js
+++ constructor-function/base/index.js
@@ -1,12 +1,105 @@
 /**
  * Topic Summary
- * 
+ *
  * 1. Creating an object using Object Literal
  * 2. Issues with Object Literal
  * 3. Constructor function
  * 4. Creating objects with constructor function
- * 5. Params in connstructor function
+ * 5. Params in constructor function
  * 6. Constructor Function Vs Object Literal ie. unique context for constuctor function
  * 7. Examples of adding properties on objects & constructor function
  * 8. Built-in constructor functions
-*/
+ */
+
+// 1.  Creating an object using Object Literal
+// const user = {
+//   name: 'John',
+//   age: 12,
+//   isStudent: true,
+
+//   getName: function () {
+//     console.log();
+//   },
+// };
+
+// const user2 = {
+//   name: 'James',
+//   age: 23,
+//   isStudent: false,
+
+//   getName: function () {
+//     console.log();
+//   },
+// };
+
+// const user3 = {
+//   name: 'James',
+//   age: 23,
+//   isStudent: false,
+
+//   getName: function () {
+//     console.log();
+//   },
+// };
+
+// 3. Constructor function
+// function User() {
+//     this.name = "John";
+//     this.age = 12,
+//     this.isMale = true
+// }
+
+// const user1 = new User();
+// const user2 = new User();
+
+// console.log(user1, user2)
+
+// // variable name => String, Number etc => As noun
+// // Function name => As Verb => addTwoNumber
+
+// // 4. Params in constructor function
+// function User(name, age, isStudent, location) {
+//   this.name = name;
+//   this.age = age;
+//   this.isStudent = isStudent;
+//   this.location = location
+// }
+
+// const john = new User('John', 12, true, "India");
+// const james = new User('James', 23, false);
+
+// console.log(john.location, james.location);
+
+// 6. Constructor Function Vs Object Literal
+// 6A. Object Literal
+// const user1 = {
+//   name: 'John',
+//   age: 12,
+// };
+
+// const user2 = user1
+
+// user1.age = 100
+
+// console.log(user1, user2)
+
+// // 6B. Constructor Function
+// function User(name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
+
+// let user3 = new User('John', 12);
+// let user4 = new User('James', 23);
+
+// user3 = null
+
+// console.log(user3, user4);
+
+
+// 7. Built-in constructor functions
+// const string = new String("SS")
+// console.log(string)
+
+// const user = new Object({})
+// console.log(user)
--- constructor-function/call-apply-bind/index.js
+++ constructor-function/call-apply-bind/index.js
@@ -1,46 +1,95 @@
 /**
  * Topic Summary
- * 
+ *
  * 1. Bind method - Creates a new function when called has its own "this"
- * 2. 
-*/
+ * 2.
+ */
 
 /** Bind Example
  * const characterDetails = {
  *  name: "James Bind"
  * }
- * 
+ *
  * const getCharacterName = function() {
  *  console.log(this.name, " is the character name")
  * }
- * 
+ *
  * const characterName = getCharacterName.bind(characterDetails)
  */
 
+// 1. Bind => Returns a function to use it with "this"
+// 1A. Created an object
+// const user = {
+//     name: "James",
+//     age: 12
+// }
+
+// // 1B. Create a function
+// const getName = function getName() {
+//     console.log(this.age)
+// }
+
+// // getName => gun
+// // user => bullet
+
+// const result = getName.bind(user)
+// result()
+
 /**
  * Call Method
- * 
+ *
  * const characterDetails = {
  *  name: "Ramadheer Singh"
  * }
- * 
+ *
  * const getCharacterDetails = function(friend) {
  *  console.log(this.name + " is friend of " + friend)
  * }
- * 
+ *
  * console.log(getCharacterDetails.call(characterDetails, "Sultan"))
  */
 
+// 2A. Created an object
+// const characterDetails = {
+//   name: 'Ramadheer Singh',
+// };
+
+// // 2B. Created an function
+// const getCharacterDetails = function (friend) {
+//   console.log(this.name + ' is a friend of ' + friend);
+// };
+
+// getCharacterDetails.call(characterDetails, 'Sultan');
+
 /**
  * Apply Method
- * 
+ *
  * const characterDetails = {
  *  name: "Ramadheer Singh"
  * }
- * 
+ *
  * const getCharacterDetails = function(friend) {
  *  console.log(this.name + " is friend of " + friend)
  * }
- * 
+ *
  * console.log(getCharacterDetails.call(characterDetails, ["Sultan"]))
  */
+
+const characterDetails = {
+  name: 'Ramadheer Singh',
+};
+
+// 2B. Created an function
+const getCharacterDetails = function (friend) {
+  console.log(this.name + ' is a friend of ' + friend);
+};
+
+getCharacterDetails.apply(characterDetails, ['Sultan']);
+
+// Apply => uses array to pass the params
+// Call => will not be using any ARRAY
+
+// Differences => Bind && Call, Apply
+
+// 1. Bind allows to call the function afterwards
+// Call & Apply needs to call immidiately
